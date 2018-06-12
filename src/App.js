ab7b674562b1e15f72ea32305d9da7e975113e38
Abhishek Kumar <abhishek.kumar@mountblue.io>
2018-06-12 22:50:37 +0530

    use the radium package for hover css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -44,11 +45,16 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color:'white',
       font: 'inherit',
       border: '2px solid blue',
       padding: '10px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -66,12 +72,24 @@ class App extends Component {
           })}
         </div> 
       );
+
+      style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
+
+    let classes = [];
+    if(this.state.persons.length <=2)
+      classes.push('red');
+    if(this.state.persons.length <= 1)
+      classes.push('bold');
 
     return (
       <div className="App">
         <h1>Hello, I'm React App</h1>
-        <p>This is working</p>
+        <p className={classes.join(' ')}>This is working</p>
         <button 
           style = {style}
           onClick={this.togglePersonHandler}>
@@ -86,4 +104,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
